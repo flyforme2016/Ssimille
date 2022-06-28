9e7bdd316585451636af9008756cf4bebd097db7
flyforme2016 <cjdfidrlwd@naver.com>
2022-06-28 20:40:29 +0900

    06.28 Alarm

---
+++ client/src/api/requestFriend.js
@@ -0,0 +1,26 @@
+import Config from 'react-native-config';
+const BASE_URL = Config.BASE_URL;
+
+const requestFriend = (myUid, otherUid) => {
+    async function addFriendApi(){
+        const requestOptions = {
+            method: 'POST',
+            redirect: 'follow',
+            body: JSON.stringify({
+              myUid: myUid,
+              otherUid: otherUid,
+            }),
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+        };
+        await fetch(`${BASE_URL}/friend/addFriend`, requestOptions)
+        .then(res => console.log('res: ', res))
+        .catch(err => console.log('err:', err));
+    }
+    addFriendApi()
+};
+  
+export default requestFriend;
+  
